c2d346cf65810b7ab850ce6b262bd7bb0911b5ec
Enayetullah Rasuli <enayetrasuli@gmail.com>
2019-05-06 14:56:44 +0200

    Project initial setup

---
+++ src/main/java/throne/springreacto/petshopspring5/PetShopSpring5Application.java
@@ -0,0 +1,13 @@
+package throne.springreacto.petshopspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopSpring5Application.class, args);
+    }
+
+}

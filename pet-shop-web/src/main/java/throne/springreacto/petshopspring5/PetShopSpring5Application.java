80d63fd40393beb13d9b11141510c628e366adfe
Enayetullah Rasuli <enayetrasuli@gmail.com>
2019-05-08 09:17:59 +0200

    Added model POJO. Closes #2.

--- pet-shop-web/src/main/java/throne/springreacto/petshopspring5/PetShopSpring5Application.java
+++ pet-shop-web/src/main/java/throne/springreacto/petshopspring5/PetShopSpring5Application.java
@@ -2,13 +2,12 @@ package throne.springreacto.petshopspring5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PetShopSpring5Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(PetShopSpring5Application.class, args);
+        SpringApplication.run(PetShopSpring5Application.class, args);
     }
 
 }
